efbcc9574aeacb597c7b25738a4b4ff2f6e5656d
czfzc <1780312044@qq.com>
2019-03-12 15:12:41 +0800

    prefinish

--- src/crossingProxy/main.java
+++ src/crossingProxy/main.java
@@ -5,55 +5,26 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class main {
 	
+	final static int PORT=1031;
+	static int sum=0;
 	
 	public static void main(String[] args) throws IOException{
-		ServerSocket ss=new ServerSocket(1031);
-//		for(int i=0;;i++){
+		
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<Runnable>(5));
+		ServerSocket ss=new ServerSocket(PORT);
+		while(true) {
 			Socket socket=ss.accept();
-			connect(socket);
-//		}
-	}
-	
-	public static void connect(Socket soc) throws IOException{
-		InputStream is=soc.getInputStream();
-		OutputStream os=soc.getOutputStream();
-		byte[] b=new byte[3];
-		is.read(b);
-		if(b[0]==5&&b[1]==1&&b[2]==0) {
-			os.write(new byte[] {5,0});
-		}
-		byte[] c=new byte[4];
-		is.read(c);
-		
-		int domainLen=is.read();
-		byte[] domainarr=new byte[domainLen];
-		is.read(domainarr);
-		String domain=new String(domainarr);
-		System.out.println(domain);
-		
-		byte[] portarr=new byte[2];
-		is.read(portarr);
-		short port=main.bytesToShort(portarr);
-		System.out.println(port);
-	//	if(c[0]==5&&c[1]==1&&c[2]==0&&c[3]==3) {
-	//		os.write(new byte[] {5,0});
-			main.printBytes(is);
-	//	}
-		
-	}
-	
-	public static short bytesToShort(byte[] b) {
-		short z=(short)(((b[0]&0x00FF)<<8)|(0x00FF& b[1]));
-		return z;
-	}
-	
-	public static void printBytes(InputStream is) throws IOException {
-		byte b=0;
-		while((b=(byte)is.read())!=-1) {
-			System.out.print((char)b);
+            Runner runner=new Runner(socket,sum++);
+            executor.execute(runner);
+            System.out.println("线程池中线程数目："+executor.getPoolSize()+"，队列中等待执行的任务数目："+
+            executor.getQueue().size()+"，已执行完别的任务数目："+executor.getCompletedTaskCount());
 		}
 	}
 	
